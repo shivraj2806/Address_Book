2c8a738df6c0b0f134caf3d8e3d7915d67ce9b52
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-17 17:38:40 +0530

    Added: Ability to edit existing contact in address book

--- src/com/Bridgelabz/Contact.java
+++ src/com/Bridgelabz/Contact.java
@@ -1,5 +1,10 @@
 package com.Bridgelabz;
+
+import java.util.Scanner;
+
 class Contact{
+
+    Scanner sc = new Scanner(System.in);
         private String firstName;
         private String lastName;
         private String address;
@@ -86,6 +91,69 @@ class Contact{
     public void display() {
         System.out.println(" FIRST NAME: " + getFirstName() + " LAST NAME: " + getLastName() + " ADDRESS: " + getAddress() + " CITY " + getCity()
                 + " STATE:" + getState() + " EMAIL ID:" + getEmail() + " ZIP CODE:" + getZipCode() + " PHONE NUMBER:" + getPhoneNumber());
+    }
+    public void editDetails() {
+
+        System.out.println(" SELECT THE OPTION YOU WANT TO EDIT : ");
+        System.out.println(" A.FIRST NAME\n B.LAST NAME\n C.ADDRESS\n D.CITY\n E.STATE\n F.EMAIL ID\n G.ZIPCODE\n H.PHONE NUMBER\n I.DELETE CONTACT ");
+
+        char option;
+        option = sc.next().charAt(0);
+        sc.nextLine();
+
+        switch (option) {
+            case 'A':
+                System.out.println("ENTER FIRST NAME: ");
+                String firstName = sc.nextLine();
+                setFirstName(firstName);
+                break;
+            case 'B':
+                System.out.println("ENTER LAST NAME: ");
+                String lastName = sc.nextLine();
+                setLastName(lastName);
+                break;
+            case 'C':
+                System.out.println("ENTER ADDRESS: ");
+                String address = sc.nextLine();
+                setAddress(address);
+                break;
+            case 'D':
+                System.out.println("ENTER CITY: ");
+                String city = sc.nextLine();
+                setCity(city);
+                break;
+            case 'E':
+                System.out.println("ENTER STATE: ");
+                String state = sc.nextLine();
+                setState(state);
+                break;
+            case 'F':
+                System.out.println("ENTER EMAIL ID: ");
+                String email = sc.nextLine();
+                setEmail(email);
+                break;
+            case 'G':
+                System.out.println("ENTER ZIPCODE: ");
+                Long zipcode = sc.nextLong();
+                setZipCode(zipcode);
+                break;
+            case 'H':
+                System.out.println("ENTER PHONE NUMBER: ");
+                Long phoneNumber = sc.nextLong();
+                setPhoneNumber(phoneNumber);
+                break;
+
+            case 'I':
+                setFirstName(null);
+                setLastName(null);
+                setAddress(null);
+                setCity(null);
+                setState(null);
+                setEmail(null);
+                setZipCode(null);
+                setPhoneNumber(null);
+
+        }
     }
     }
 
--- src/com/Bridgelabz/Contact_Main.java
+++ src/com/Bridgelabz/Contact_Main.java
@@ -7,29 +7,32 @@ public class Contact_Main {
         getInput();
     }
 
-        public static void getInput () {
-            Scanner input = new Scanner(System.in);
+    public static void getInput() {
+        Scanner input = new Scanner(System.in);
 
-            System.out.print("ENTER THE FIRST NAME: ");
-            String firstName = input.nextLine();
-            System.out.print("ENTER THE LAST NAME: ");
-            String lastName = input.nextLine();
-            System.out.print("ENTER THE ADDRESS: ");
-            String address = input.nextLine();
-            System.out.print("ENTER THE CITY: ");
-            String city = input.nextLine();
-            System.out.print("ENTER THE STATE: ");
-            String state = input.nextLine();
-            System.out.print("ENTER THE EMAIL ID: ");
-            String email = input.nextLine();
-            System.out.print("ENTER THE ZIPCODE: ");
-            Long zipcode = input.nextLong();
-            System.out.print("ENTER THE PHONE NUMBER: ");
-            Long phoneNumber = input.nextLong();
+        System.out.print("ENTER THE FIRST NAME: ");
+        String firstName = input.nextLine();
+        System.out.print("ENTER THE LAST NAME: ");
+        String lastName = input.nextLine();
+        System.out.print("ENTER THE ADDRESS: ");
+        String address = input.nextLine();
+        System.out.print("ENTER THE CITY: ");
+        String city = input.nextLine();
+        System.out.print("ENTER THE STATE: ");
+        String state = input.nextLine();
+        System.out.print("ENTER THE EMAIL ID: ");
+        String email = input.nextLine();
+        System.out.print("ENTER THE ZIPCODE: ");
+        Long zipcode = input.nextLong();
+        System.out.print("ENTER THE PHONE NUMBER: ");
+        Long phoneNumber = input.nextLong();
 
-            Contact person = new Contact(firstName, lastName, address, city, state, email, zipcode, phoneNumber);
-            person.display();
-        }
+        Contact person = new Contact(firstName, lastName, address, city, state, email, zipcode, phoneNumber);
+        person.display();
+
+        person.editDetails();
+        person.display();
     }
+}
 
 
